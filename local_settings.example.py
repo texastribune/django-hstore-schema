dbcc94a085e056aa2fdaa1cf36a58864c1d190f4
Noah Seger <nosamanuel@gmail.com>
2012-07-25 17:27:22 -0500

    Move local settings into example file

---
+++ local_settings.example.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django_hstore.postgresql_psycopg2',
+        'NAME': 'tribdata',
+        'USER': 'tribdata',
+        'PASSWORD': 'trib101',
+        'OPTIONS': {
+            'autocommit': True
+        },
+    },
+}
