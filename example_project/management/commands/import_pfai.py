76ebb899d95703cf32d7506357cc6a134119bdef
Noah Seger <noah@texastribune.org>
2012-11-09 13:28:02 -0600

    Start from scratch

--- example_project/management/commands/import_pfai.py
+++
@@ -1,8 +0,0 @@
-from example_project.utils import import_pfai_file
-
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-    def handle(self, path, **options):
-        import_pfai_file(path)
