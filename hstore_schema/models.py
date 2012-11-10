f7882be1ee2dc842880ddccb70788b445d73c052
Noah Seger <noah@texastribune.org>
2012-11-09 19:01:21 -0600

    Implement data model from latest mock

--- hstore_schema/models.py
+++ hstore_schema/models.py
@@ -1,2 +1,147 @@
+from uuid import UUID
+
 from django.db import models
 from django_hstore import hstore
+import jsonfield
+
+
+class Bucket(models.Model):
+    """
+    A namespace for data uniquely identified by its `slug`.
+    """
+    name = models.CharField(max_length=255)
+    short_name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+
+class Keymap(models.Model):
+    """
+    Maps a raw value to a key for a given namespace and keyspace.
+
+    `namespace`: represents an entity type, like 'campus' or 'district'
+    `keyspace`: determines the raw key source (i.e. `FIPS` or `CDC`)
+    `raw`: the literal key value from the raw data
+    `key`: the final mapped value used to key records and data
+    """
+    bucket = models.ForeignKey(Bucket, related_name='keymaps')
+
+    namespace = models.SlugField()
+    keyspace = models.SlugField()
+
+    raw = models.CharField(max_length=255)
+    key = models.SlugField(max_length=255)
+
+    class Meta:
+        unique_together = ('bucket', 'namespace', 'keyspace', 'raw', 'key')
+
+
+class Source(models.Model):
+    """
+    A named source of data with some simple metadata.
+    """
+    bucket = models.ForeignKey(Bucket, related_name='sources')
+
+    name = models.CharField(max_length=255)
+    short_name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255)
+    readme = models.TextField(blank=True, null=True)
+    attribution = models.CharField(max_length=255, blank=True, null=True)
+
+
+class Revision(models.Model):
+    """
+    A version of dataset that contains the sets of `Data` and `Record`s
+    as they existed at particular time.
+
+    The revision with no `next` revision contains the current dataset.
+
+    `digest` is a UUID4 that is shared between all revisions that are
+    run as part of the same process.
+    """
+    bucket = models.ForeignKey(Bucket, related_name='revisions')
+
+    digest = models.CharField(max_length=32)
+    next = models.OneToOneField('self', null=True, related_name='previous')
+
+    @property
+    def time(self):
+        return UUID(self.digest).time
+
+
+class Dataset(models.Model):
+    """
+    A named set of data that is sourced from raw files.
+    """
+    source = models.ForeignKey(Source, related_name='datasets')
+    revision = models.ForeignKey(Revision, related_name='datasets')
+    version = models.CharField(max_length=255)
+
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, unique=True)
+    raw = models.FileField(upload_to='datasets', blank=True, null=True)
+
+
+class Record(models.Model):
+    """
+    Stores a raw record of data from a data source, such as a row from
+    a CSV file or a JSON object.
+
+    `data`: a dictionary of values keyed by the raw header
+    `order`: the row number or list index in the raw file
+    """
+    dataset = models.ForeignKey(Dataset, related_name='records')
+
+    key = models.SlugField(max_length=255, blank=True, null=True)
+    data = hstore.DictionaryField()
+    order = models.IntegerField()
+
+
+class Field(models.Model):
+    """
+    A named field available within a dataset.
+
+    `name`: the display name for the field
+    `label`: the field used to label the data in a resulting Schema
+    """
+    dataset = models.ForeignKey(Dataset, null=True, related_name='fields')
+
+    raw_name = models.CharField(max_length=255)
+    display_name = models.CharField(max_length=255, blank=True, null=True)
+
+
+class Data(models.Model):
+    """
+    Stores a typed field/value pair for a given key within a namespace.
+    """
+    revision = models.ForeignKey(Revision, related_name='dataset')
+    version = models.CharField(max_length=255)
+
+    key = models.SlugField()
+    label = models.CharField(max_length=255)
+    value = jsonfield.JSONField()
+    facets = hstore.DictionaryField(blank=True, null=True)
+    primary = models.BooleanField(default=False)
+
+
+class Code(models.Model):
+    """
+    The code used to transform data.
+    """
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+
+
+class Transformation(models.Model):
+    """
+    The entire transformation applied to the data, including the source
+    records and a reference to the code that was used to modify them.
+
+    `source_field`: the direct source of the target primary Data
+    `code`: the code used to tranform Records into secondary Data
+    """
+    revision = models.ForeignKey(Revision)
+
+    data = models.OneToOneField(Data, related_name='transformation')
+    records = models.ForeignKey(Record, related_name='transformations')
+    source_field = models.CharField(max_length=255, blank=True, null=True)
+    code = models.ForeignKey(Code, null=True)
