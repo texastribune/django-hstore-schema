98fa66d1fc7c4da2ba71d832bfc9aa8906c55b87
Noah Seger <noah@texastribune.org>
2012-11-12 10:19:45 -0600

    Update database settings

--- local_settings.example.py
+++ local_settings.example.py
@@ -1,11 +1,8 @@
 DATABASES = {
     'default': {
         'ENGINE': 'django_hstore.postgresql_psycopg2',
-        'NAME': 'tribdata',
-        'USER': 'tribdata',
-        'PASSWORD': 'trib101',
-        'OPTIONS': {
-            'autocommit': True
-        },
+        'NAME': 'hstoreschema',
+        'USER': 'hstoreschema',
+        'OPTIONS': {'autocommit': True},
     },
 }
