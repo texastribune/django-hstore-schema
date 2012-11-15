1e63c3dbc5de151d41278e6e9d2c4df4154c95c4
Noah Seger <noah@texastribune.org>
2012-11-15 13:57:52 -0600

    Separate headers from data to save space

--- hstore_schema/management/commands/load_dataset.py
+++ hstore_schema/management/commands/load_dataset.py
@@ -25,7 +25,9 @@ def preview_file(dataset, register, csv):
         base_name = os.path.basename(csv)
         label, ext = os.path.splitext(base_name)
         reader = CSVKitDictReader(f)
-        for data in reader:
+        dataset.fields = reader.fieldnames
+        for row in reader:
+            data = dict([(i, row[f]) for i, f in enumerate(reader.fieldnames)])
             record = Record(dataset=dataset, data=data, label=label,
                             order=reader.line_num)
             register.process(record)
