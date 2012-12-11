20a45e972633d31932d758514db19bd114071a3e
Noah Seger <noah@texastribune.org>
2012-12-11 17:54:11 -0600

    Add facets and data for commended and met-standard datasets

--- example_project/data/aeis/taks.py
+++ example_project/data/aeis/taks.py
@@ -76,6 +76,8 @@ def facet_subject(record, field, value):
 
 
 @campus_taks.facet('gender')
+@campus_taks_commended.facet('gender')
+@campus_taks_met_standard.facet('gender')
 def facet_gender(record, field, value):
     if 'Male' in field:
         return 'Male'
@@ -93,6 +95,10 @@ def facet_language(record, field, value):
 
 # TODO: Map/reduce Asian and Pacific Islander aggregate ethnicities
 @campus_taks.field_data('passing_rate', label='Passing Rate',
+        namespace='campus', pattern=r'.* Rate$')
+@campus_taks_commended.field_data('commended_rate', label='Commended Rate',
+        namespace='campus', pattern=r'.* Rate$')
+@campus_taks_met_standard.field_data('passing_rate', label='Passing Rate',
         namespace='campus', pattern=r'.* Rate$')
 def get_taks_value(record, field, value, facets=None):
     masked = False
